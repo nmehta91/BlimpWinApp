e1905de8e4fc653fc5b4f1bbd6d072dfa621b5ba
Nitish Mehta <nmehta91@ucla.edu>
2016-10-07 23:28:55 -0700

    group layout addition to run log updated

--- src/RunLogsWindow.java
+++ src/RunLogsWindow.java
@@ -26,6 +26,9 @@ import javax.swing.JFileChooser;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class RunLogsWindow extends JFrame {
 
@@ -44,7 +47,7 @@ public class RunLogsWindow extends JFrame {
 	public RunLogsWindow(String pathToExe) {
 		setTitle("Output");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 791, 490);
+		setBounds(100, 100, 786, 477);
 		
 		model = SyntaxModel.getInstance();
 		this.pathToExe = pathToExe;
@@ -52,7 +55,6 @@ public class RunLogsWindow extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
 
 		JFrame frame = this;
 		ImageIcon img = new ImageIcon("Resources\\blimplogo_32x32.png");
@@ -64,13 +66,8 @@ public class RunLogsWindow extends JFrame {
 		logTextArea.setFont(new Font("Courier", Font.PLAIN, 14));
 		
 		JScrollPane scrollPane_1 = new JScrollPane(logTextArea);
-		scrollPane_1.setBounds(5, 5, 760, 410);
-
-		contentPane.add(scrollPane_1, BorderLayout.CENTER);
 		
 		progressBar = new JProgressBar();
-		progressBar.setBounds(602, 426, 163, 14);
-		contentPane.add(progressBar);
 		
 		JButton btnSaveOutput = new JButton("Save Output");
 		btnSaveOutput.addActionListener(new ActionListener() {
@@ -99,8 +96,29 @@ public class RunLogsWindow extends JFrame {
 				}
 			}
 		});
-		btnSaveOutput.setBounds(10, 426, 118, 23);
-		contentPane.add(btnSaveOutput);
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addComponent(scrollPane_1, GroupLayout.DEFAULT_SIZE, 760, Short.MAX_VALUE)
+					.addGap(5))
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addComponent(btnSaveOutput, GroupLayout.PREFERRED_SIZE, 118, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 469, Short.MAX_VALUE)
+					.addComponent(progressBar, GroupLayout.PREFERRED_SIZE, 163, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(scrollPane_1, GroupLayout.DEFAULT_SIZE, 410, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(progressBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnSaveOutput))
+					.addGap(20))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
 	
 	public void initiateExecution()
