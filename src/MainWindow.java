bc12637c66d65c82e4ff96f4db2bd4a8c6d2bf75
Nitish Mehta <nmehta91@ucla.edu>
2016-11-09 01:40:27 -0800

    after associating .imp files with blimp.exe, it opens up existing syntax files upon double clicking

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -81,6 +81,9 @@ public class MainWindow {
 				try {
 					MainWindow window = new MainWindow();
 					window.frame.setVisible(true);
+					if(args.length > 0) {
+						window.openFile(new File(args[0]), 0);
+					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
